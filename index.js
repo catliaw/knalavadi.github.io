d21d42ad295f8871693a04d1371da2b2b29899ca
knalavadi <krisha.nalavadi@gmail.com>
2015-10-30 13:51:49 -0700

    adding javascript file

---
+++ index.js
@@ -0,0 +1,11 @@
+<SCRIPT LANGUAGE="Javascript">
+<!-- 
+
+var arrowdownoverlay = document.getElementById('arrowdownoverlay');
+var video= document.getElementById('video');
+video.addEventListener('progress', function() {
+    var show= video.currentTime>=5 && video.currentTime<10;
+    overlay.style.visibility= show? 'visible' : 'visible';
+  }, false);
+-->
+</SCRIPT>
