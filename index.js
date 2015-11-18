0f69162744e17af402e586ab2070cf81620cc36f
knalavadi <krisha.nalavadi@gmail.com>
2015-11-17 16:26:50 -0800

    javascript header changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// social hover over magnify 
 	function bigIMG(x){
 		x.style.height = "35px";
 		x.style.width ="35px";
@@ -8,6 +8,29 @@
 		x.style.height = "25px";
 		x.style.width = "25px";
 	}	
+
+
+// header scroll down disappear 
+$(window).scroll(
+    {
+        previousTop: 0
+    }, 
+    function () {
+    var currentTop = $(window).scrollTop();
+    if (currentTop < this.previousTop) {
+        $(".sidebar em").text("Up"); 
+        $(".navbarnavbar-default").show();
+    } else {
+        $(".sidebar em").text("Down");
+        $(".navbarnavbar-default").hide();
+    }
+    this.previousTop = currentTop;
+});
+
+
+
+
+// project image -text roll over 
 
 
 $(document).ready(main);
