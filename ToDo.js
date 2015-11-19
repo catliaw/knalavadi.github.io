ccb085a70f302cb83962079341f58a9377a4bf19
knalavadi <krisha.nalavadi@gmail.com>
2015-11-18 18:56:04 -0800

    todo javascript file

---
+++ ToDo.js
@@ -0,0 +1,5 @@
+var form = document.getElementById("the-form");
+var item = document.getElementById("item");
+var list = document.getElementById("todo-list");
+
+item.addEventListener("submit", addToDoItem);
