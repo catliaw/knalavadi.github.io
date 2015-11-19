39283c1983f5643d845a222144009fa78667e41b
knalavadi <krisha.nalavadi@gmail.com>
2015-11-19 12:54:21 -0800

    finished first draft

--- List.js
+++ List.js
@@ -1,1 +1,8 @@
-document.getElementByID("add").onclick = function 
+document.getElementByID("add").onclick = function(){
+	var text = document.getElementById("todo").value;
+	var ul = document.getElementByID("list");
+	var listitem = document.getElement("li");
+	listitem.innerText = text;
+	document.body.appendChild(listitem);
+	document.getElementById('todo').value = ""
+}
