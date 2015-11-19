2e2358c3da756da8f35e053ea77a5301768c5ac8
krisha <knalavadi@users.noreply.github.com>
2015-11-18 19:02:14 -0800

    Delete ToDo.js

--- ToDo.js
+++
@@ -1,5 +0,0 @@
-var form = document.getElementById("the-form");
-var item = document.getElementById("item");
-var list = document.getElementById("todo-list");
-
-item.addEventListener("submit", addToDoItem);
