3dfad6384390f1804e35e448b7aecb08cb0ef2ea
knalavadi <krisha.nalavadi@gmail.com>
2015-11-23 17:54:27 -0800

    committing list

--- List.js
+++ List.js
@@ -1,7 +1,7 @@
-document.getElementByID("add").onclick = function(){
+document.getElementById("add").onclick = function(){
 	var text = document.getElementById("todo").value;
-	var ul = document.getElementByID("list");
-	var listitem = document.getElement("li");
+	var a = document.getElementById("list");
+	var listitem = document.createElement("li");
 	listitem.innerText = text;
 	document.body.appendChild(listitem);
 	document.getElementById('todo').value = ""
