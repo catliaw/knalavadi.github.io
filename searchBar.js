56730efdc285875746b0de6fc180428ccbaaef76
knalavadi <krisha.nalavadi@gmail.com>
2015-12-01 16:42:58 -0800

    added random fact/tweet generator

--- searchBar.js
+++ searchBar.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
-	var count = 0;
-	$('textarea').keyupfunction(){
-		count = $(this).val().length;
-		$(char-count).text(count);
+		var count = 0;
+		$('textarea').keyup(function(){
+			count = $(this).val().length;
+			$('char-count').text(count);
+
+			if(count <140){
+				$('.response').text("tweetable")
+			}
+			
+			if(count >140 && count <160){
+				$('.response').text("textable");
+			}	
+			if(count > 160){
+				$('.response').text("send an email");
+			}
+
+		})
+	$('#tweetButton').click(function(){
+		$.get('http://numbersapi.com/random/trivia', function(data){
+		console.log('random tweet:', data);
+		$('#randomTweet').text(data);
+		});
+	});
+
+});
+
+// http://eviltrailmix.com/tweet/
+				
 		
-	}
-})
+	
+// });
+// $(document).ready(main)
