24699d38d965a3fc5e7eccb35e148f9d5ff4f70a
catliaw <cat.liaw@gmail.com>
2015-12-20 19:33:13 -0800

    Took out extra code and added notes.

--- testgame2.js
+++ testgame2.js
@@ -1,138 +1,114 @@
 $(document).ready(function(){
 
+  //DEFINE FUNCTIONS
 
-
-      function twoCardsFaceUp(){
-        return $(".upcard").length == 2;
-      }
-      
-      function cardsMatch(){
-        return $(".upcard:eq(0)").text() == $(".upcard:eq(1)").text();
-      };
-      
-      function markCardsAsMatched(){
-        $(".upcard").each(function(){
-          $(this).addClass("matched").removeClass("upcard").off("click");
-        });
-        console.log("matched!");
-      };
-      
-      // function updateScore(player){
-      //   var el = $(player).find(".noPairs");
-      //   var p = Number(el.text());
-      //   el.text(p+1);
-      // }
-      
-      function allCardsMatched(){
-        return ($(".matched").length == 16)
-      };
-      
-      function flipCardsBackOver(){
-        setTimeout(function(){
-          $(".upcard").each(function(){
-            $(this).addClass("downcard").removeClass("upcard");
-          });
-        }, 1000);
-      }
-      
-      function shuffle(cards){
-        // Uses Fisher&#8211;Yates shuffle
-        // See http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
-        var i = cards.length, j, tempi, tempj;
-        if ( i == 0 ) return false;
-        while ( --i ) {
-          j = Math.floor( Math.random() * ( i + 1 ) );
-          tempi = cards[i];
-          tempj = cards[j];
-          cards[i] = tempj;
-          cards[j] = tempi;
-        }
-        return cards;
-      }
-      
-      // function highlightCurrentPlayer(player){
-      //   $("#playerInfo p").each(function(){
-      //     if ($(this).hasClass("active")){
-      //       $(this).removeClass("active");
-      //     }
-      //   });
-      //   $(player).find("p").first().addClass("active");
-      // }
-      
-      function incrementTurns(player){
-        var el = $(player).find(".noTurns");
-        var t = Number(el.text());
-        el.text(t+1);
-      }
-      
-      // function updateCurrentPlayer(player){
-      //   currentPlayer = (currentPlayer.match(/1/))? "#player2" : "#player1"
-      // }
-      
-      // function winner(){
-      //   var playerOnePoints = Number($("#player1").find(".noPairs").text());
-      //   var playerTwoPoints = Number($("#player2").find(".noPairs").text());
-      //   if (playerOnePoints > playerTwoPoints){
-      //     return "Player one won!";
-      //   } else if (playerOnePoints < playerTwoPoints){
-      //     return "Player two won!";
-      //   } else {
-      //     return "An honourable draw!";
-      //   }
-      // }
-      
-      // Shuffle cards
-      var cards = $(".card");
-      cards.remove();
-      cards = shuffle(cards);
-      cards.appendTo($("#Playfield"));
-      
-      // Main Loop
-      $('.card').on("click", function(){
-        if ($(".upcard").length == 2){
-          return false;
-        }
+  //Function that returns if 2x of class "upcard" in the HTML code
+  function twoCardsFaceUp(){
+    return $(".upcard").length == 2;
+  }
   
-        $(this).removeClass("downcard").addClass("upcard");
-        
-        if (twoCardsFaceUp()){
-          incrementTurns(currentPlayer);
-          if (cardsMatch()){
-            markCardsAsMatched()
-            if (allCardsMatched()){
-              $('#winText').text('You Won!');
-              clock.stop();
-              console.log('all cards matched!');
-            }
-          }else{
-            flipCardsBackOver();
-            updateCurrentPlayer(currentPlayer);
-            setTimeout(function(){highlightCurrentPlayer(currentPlayer)}, 1000);
-          }
+  //Function that returns if the 2 class "upcard" cards match (number on the card which is in text)
+  function cardsMatch(){
+    return $(".upcard:eq(0)").text() == $(".upcard:eq(1)").text();
+  };
+  
+  //Function that gives matching cards a class of "matched", removes class "upcard", and turns of "click" eventcaller
+  function markCardsAsMatched(){
+    $(".upcard").each(function(){
+      $(this).addClass("matched").removeClass("upcard").off("click");
+    });
+    console.log("matched!");
+  };
+  
+  //Function that returns if all cards have class matched (16 cards, so 16 class "matched" in HTML code)
+  function allCardsMatched(){
+    return ($(".matched").length == 16)
+  };
+  
+  //Function that flips card over by adding the class "downcard" and removing the class "upcard"
+  function flipCardsBackOver(){
+    setTimeout(function(){
+      $(".upcard").each(function(){
+        $(this).addClass("downcard").removeClass("upcard");
+      });
+    }, 1000);
+  }
+
+  function shuffle(cards){
+    // Uses Fisher-Yates shuffle
+    // See http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+    var i = cards.length, j, tempi, tempj;
+    if ( i == 0 ) return false; //If the number of cards (with "cards" class... defined below) = 0 
+    while ( --i ) { // Decrementing i by 1 per loop
+      j = Math.floor( Math.random() * ( i + 1 ) );
+      tempi = cards[i];
+      tempj = cards[j];
+      cards[i] = tempj;
+      cards[j] = tempi; //shuffle 2 times?
+    }
+    return cards;
+  }
+  //END FUNCTION DEFINITONS
+  
+  //START JS MANIPULATIONS
+
+  // Shuffle cards
+  var cards = $(".card"); //variable cards = elements in HTML with "cards" class
+  cards.remove(); //remove elements with "card" class from HTML
+  cards = shuffle(cards); //put elements with "card" class in shuffle function
+  cards.appendTo($("#Playfield")); //Add the shuffled "card" classes to "Playfield" id (<div>)
+  
+  // Main Loop
+  $('.card').on("click", function(){
+    //Limits clicks allowed to 2, will not run loop if more than 2 classes of "upcard"
+    if ($(".upcard").length == 2){
+      return false;
+    }
+    //If less than 2 cards already have "upcard" class, then remove "downcard" class and add "upcard" class
+    $(this).removeClass("downcard").addClass("upcard");
+    
+    //If 2 cards have class of "upcard"
+    if (twoCardsFaceUp()){
+      //If these 2 cards have matching text (and therefor matching background images)
+      if (cardsMatch()){
+        //then mark cards as matched (add "matched" class, remove "upcard" class, turn off click funtionality)
+        markCardsAsMatched()
+        //then if all cards are matched (16x "upcard" class present in HTML)
+        if (allCardsMatched()){
+          //Set "You won" text
+          $('#winText').text('You Won!');
+          //Stop the flipclock
+          clock.stop();
+          console.log('all cards matched!');
         }
-      });
-      
-      $("#reset").click(function(){
-        location.reload();
-      });
-      
-      $(".card").each(function(){
-        var num = $(this).data("face");
-        $(this).text(num);
-      });
-      
-      var clock = $('.your-clock').FlipClock(0, {
-          clockFace: 'MinuteCounter',
-          autoStart: false  
-      });
+      }else{
+        //else (not matching), flip cards back over (add "downcard" class and remove "upcard" class)
+        flipCardsBackOver();
+      }
+    }
+  });
+  
+  //Refreshes the page when click on link with "reset" id
+  $("#reset").click(function(){
+    location.reload();
+  });
+  
+  //Extracts the data stored in class "data-face" and prints the value in the corresponding <div> as text
+  $(".card").each(function(){
+    var num = $(this).data("face");
+    $(this).text(num);
+  });
+  
+  //Setting flipclock to start at 0, minute counter interface, and not autostart.
+  var clock = $('.your-clock').FlipClock(0, {
+      clockFace: 'MinuteCounter',
+      autoStart: false  
+  });
 
-      $('#Playfield').one("click", function(){
-        clock.start();
-      });
-
-      var currentPlayer = "#player1";
-      highlightCurrentPlayer(currentPlayer);
+  //Setting the flipclock to start when any area within #Playfield <div> is clicked
+  $('#Playfield').one("click", function(){
+    clock.start();
+  });
 
 
-
-      });
+});
